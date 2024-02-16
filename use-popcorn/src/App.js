49c4da9e9b9061edb8d2aa174ce20face0c48206
Pablo <pablito@me.com>
2024-02-16 12:06:50 +0000

    Fixed error in retrieving local storage data where there was no data to retrieve and map caused an error.

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import StarRating from './StarRating';
 
 const average = (arr) =>
@@ -13,8 +13,8 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
   
   const [watched, setWatched] = useState(() => {
-    const storedValue = localStorage.getItem('watched');
-    return JSON.parse(storedValue)
+    const storedValue = JSON.parse(localStorage.getItem('watched'));
+    return storedValue || [];
   });
 
   const handleSelectMovie = (id) => {
@@ -27,8 +27,6 @@ export default function App() {
 
   const handleAddWatchedMovie = (movie) => {
     setWatched(watched => [...watched, movie]);
-
-    // localStorage.setItem('watched', JSON.stringify([...watched, movie]))
   }  
 
   const handleDeleteWatched = (id) => {
@@ -138,11 +136,22 @@ function NumResults({ movies }) {
 }
 
 function Search({ query, setQuery }) {
-  
+
+  const inputEl = useRef(null);
+
   useEffect(() => {
-    const inputEl = document.querySelector('.search');
-    inputEl.focus();
-  }, [])
+    const callback = (e) => {
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+      }
+    } 
+
+    document.addEventListener('keydown', callback);
+    
+    return () => {
+      document.removeEventListener('keydown', callback);
+    }
+  }, []);
 
   return (
     <input
@@ -151,6 +160,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
