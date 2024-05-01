f45b4d518c9ac8aae23ee599a907d1e5ae0afcf7
Pablo <pablito@me.com>
2024-05-01 18:53:18 +0100

    Starting first advanced section project. Chapter 187.

---
+++ react-quiz/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DateCounter from './components/DateCounter';
+
+export default function App() {
+  return (
+    <div className="App">
+      <DateCounter />
+    </div>
+  );
+}
+
