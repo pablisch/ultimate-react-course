b44020b9e9abe2fd8aea841026b23868eada17a1
Pablo <pablito@me.com>
2024-05-02 20:04:13 +0100

    Implemented first quiz question logic.

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,11 +1,95 @@
 import './App.css';
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Error from './components/Error';
+import Loader from './components/Loader';
+import StartScreen from './components/StartScreen';
+import Question from './components/Question';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'startQuiz':
+      return {
+        ...state,
+        status: 'active',
+      };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    default:
+      throw new Error('Unknown action type');
+  }
+};
 
 export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/questions');
+        const data = await response.json();
+        console.log(data);
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+        console.log('Error:', error);
+      }
+    };
+
+    getQuestions();
+  }, []);
+
   return (
-    <div className="App">
-      <DateCounter />
+    <div className='app'>
+      <Header />
+      <Main className='main'>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
     </div>
   );
 }
-
