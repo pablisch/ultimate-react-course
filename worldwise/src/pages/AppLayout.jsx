fb4fc6b6d31d352ed71b8a381468df7d8e0f50c3
Pablo <pablito@me.com>
2024-05-07 11:11:09 +0100

    Added starter components from course repo starter folder.

---
+++ worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+
+import AppNav from "../components/AppNav"
+
+const AppLayout = () => {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  )
+}
+
+export default AppLayout
--- worldwise/src/pages/PageNotFound.jsx
+++ worldwise/src/pages/PageNotFound.jsx
@@ -1,11 +1,7 @@
-// import React from 'react'
-
-const PageNotFound = () => {
+export default function PageNotFound() {
   return (
     <div>
-      404
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
-
-export default PageNotFound
